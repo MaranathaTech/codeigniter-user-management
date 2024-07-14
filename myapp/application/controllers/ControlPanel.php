3f3e60bfb7a5d3bcfe5978aa9b2dd31bfcf8a5d9
Ernie Lail <37405567+MaranathaTech@users.noreply.github.com>
2024-07-14 17:58:28 -0400

    Tidy Up

--- myapp/application/controllers/ControlPanel.php
+++ myapp/application/controllers/ControlPanel.php
@@ -6,19 +6,12 @@ class ControlPanel extends CI_Controller {
 
 
 	public function __construct(){
-		
 		parent::__construct();
 
-	    if($this->session->userdata('logged_in') === TRUE){
-
-
-    	}
-    	else{
-
-    		redirect('user');
-
-    	}
-    	
+		if($this->session->userdata('logged_in') !== TRUE){
+	    		redirect('user');
+	    	}
+    
 	}
 
 
@@ -26,21 +19,15 @@ class ControlPanel extends CI_Controller {
 		
 		//check user permissions to load admin header
 		if($this->session->userdata('level') == 1){
-			
 			$this->load->view('templates/admin-header');
-
 		}
 		//check user permissions to load standard user
 		else if($this->session->userdata('level') == 3){
-
 			$this->load->view('templates/user-header');
-
 		}
 		//check user permissions to load base header
 		else{
-
 			$this->load->view('templates/header');
-
 		}
 
 		$this->load->view('pages/control-panel');
