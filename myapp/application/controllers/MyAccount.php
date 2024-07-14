a8a2a5e5f6e03ebaab360be9178eeb4257d2b501
Ernie Lail <37405567+MaranathaTech@users.noreply.github.com>
2024-07-14 17:59:38 -0400

    Tidy formatting

--- myapp/application/controllers/MyAccount.php
+++ myapp/application/controllers/MyAccount.php
@@ -4,9 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class MyAccount extends CI_Controller {
 
-
 	public function __construct(){
-		
 		parent::__construct();
 
 		if($this->session->userdata('logged_in') !== TRUE){
@@ -14,35 +12,27 @@ class MyAccount extends CI_Controller {
     		}
 
     		$this->load->model('User_model');
-    	
 	}
-
 
 	public function index(){
 		
 		//check user permissions to load admin header
 		if($this->session->userdata('level') == 1){
-			
 			$this->load->view('templates/admin-header');
-
 		}
 		//check user permissions to load standard user
 		else if($this->session->userdata('level') == 3){
-
 			$this->load->view('templates/user-header');
-
 		}
 		//check user permissions to load base header
 		else{
-
 			$this->load->view('templates/header');
-
 		}
 
 		$this->load->view('pages/account');
 		$this->load->view('templates/footer');	
 
-    }
+    	}
 
 	//function to process update form submissions
 	public function update(){
@@ -54,19 +44,13 @@ class MyAccount extends CI_Controller {
 		$validate = $this->User_model->update($email, $pass, $org);
 		
 		if($validate === TRUE ){//update successful
-				
 			echo $this->session->set_flashdata('msg','Account information updated successfully!');
 			redirect('myaccount');
-
 		}
-		else{//update failed
-				
+		else{//update failed	
 			echo $this->session->set_flashdata('msg','Account update failed! Please contact support.');
 			redirect('myaccount');
-
 		}
-		
 	}
-
 
 }
