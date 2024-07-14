c084b2e02e2e1fbc0bc243f4c699bdbd8df3a43f
Ernie Lail <37405567+MaranathaTech@users.noreply.github.com>
2024-07-14 18:01:29 -0400

    Tidy formatting

--- myapp/application/controllers/User.php
+++ myapp/application/controllers/User.php
@@ -6,12 +6,10 @@ class User extends CI_Controller {
 
 
 	public function __construct(){
-		
 		parent::__construct();
 
-    	$this->load->model('User_model');
-    	$this->load->model('Reset_model');
-
+    		$this->load->model('User_model');
+    		$this->load->model('Reset_model');
 	}
 
 
@@ -19,88 +17,78 @@ class User extends CI_Controller {
 		
 		if($this->session->userdata('logged_in') === TRUE){
 			redirect('controlpanel');
-    	}
+    		}
 
 		//check user permissions to load admin header
 		if($this->session->userdata('level') == 1){
-			
 			$this->load->view('templates/admin-header');
-
 		}
 		//check user permissions to load standard user
 		else if($this->session->userdata('level') == 3){
-
 			$this->load->view('templates/user-header');
-
 		}
 		//check user permissions to load base header
 		else{
-
 			$this->load->view('templates/header');
-
 		}
 
 		$this->load->view('pages/user');
 		$this->load->view('templates/footer');	
-
 	}
 
 
 	private function generateID(){
-		
 		$dt = new DateTime();
 		$ts =  $dt->getTimestamp(); 
 		$id = substr(str_shuffle('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'),1,20) . $ts . substr(str_shuffle('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'),1,20);
 
 		return $id;
-
 	}
 
 
 	public function reset(){
 		//generate email to reset password
 
-        $this->load->config('email');
-        $this->load->library('email');
-
-	$id = $this->generateID();
-        
-        $from = $this->config->item('smtp_user');
-        $to = $this->input->post('user_email');
-        $subject = 'Password Reset Confirmation';
-
-        $resetURL = "https://" . $_SERVER['SERVER_NAME'] . '/user/reset_confirmation?id=' . $id . '&email=' . $to;
-
-        $message = 'This password reset email was requested via our website. Please use the link below to reset your password. /r/r';
-        $message .= $resetURL . '/r/r';
-        $message .= 'If you did not request this email, please contact us.';
-
-        $this->email->set_newline("\r\n");
-        $this->email->from($from);
-        $this->email->to($to);
-        $this->email->subject($subject);
-        $this->email->message($message);
-
-        //write reset link to db
-	$this->Reset_model->storeResetLink($to, $id);
-
-        if ($this->email->send()) {
-			
-		echo $this->session->set_flashdata('msg','Your password reset email has been sent. Please check your email for the link to finish the password reset.');
-
-		redirect('user');
-
-        } else {
-            
-		show_error($this->email->print_debugger());
-
-        }
+	        $this->load->config('email');
+	        $this->load->library('email');
+	
+		$id = $this->generateID();
+	        
+	        $from = $this->config->item('smtp_user');
+	        $to = $this->input->post('user_email');
+	        $subject = 'Password Reset Confirmation';
+	
+	        $resetURL = "https://" . $_SERVER['SERVER_NAME'] . '/user/reset_confirmation?id=' . $id . '&email=' . $to;
+	
+	        $message = 'This password reset email was requested via our website. Please use the link below to reset your password. /r/r';
+	        $message .= $resetURL . '/r/r';
+	        $message .= 'If you did not request this email, please contact us.';
+	
+	        $this->email->set_newline("\r\n");
+	        $this->email->from($from);
+	        $this->email->to($to);
+	        $this->email->subject($subject);
+	        $this->email->message($message);
+	
+	        //write reset link to db
+		$this->Reset_model->storeResetLink($to, $id);
+	
+	        if ($this->email->send()) {
+				
+			echo $this->session->set_flashdata('msg','Your password reset email has been sent. Please check your email for the link to finish the password reset.');
+	
+			redirect('user');
+	
+	        } else {
+	            
+			show_error($this->email->print_debugger());
+	
+	        }
 
 	}
 
 
 	public function reset_confirmation(){
-		
 		$email = $this->input->get('email', TRUE);
 		$id = $this->input->get('id', TRUE);
 
@@ -137,7 +125,6 @@ class User extends CI_Controller {
 
 	//function to process registraion form submissions
 	public function register(){
-		
 		$email = $this->input->post('user_email', TRUE);
 		$pass = md5($this->input->post('user_password', TRUE));
 
@@ -183,7 +170,6 @@ class User extends CI_Controller {
 
 	//function to process login form submissions
 	public function login(){
-		
 		$email = $this->input->post('user_email', TRUE);
 		$pass = md5($this->input->post('user_password', TRUE));
 		$validate = $this->User_model->validate($email, $pass);
@@ -214,12 +200,10 @@ class User extends CI_Controller {
 
 
 	public function logout(){
-		
-    	$this->session->set_userdata('logged_in', FALSE);
+    		$this->session->set_userdata('logged_in', FALSE);
 		$this->session->sess_destroy();
 
 		redirect('user');
-
 	}
 
 }
