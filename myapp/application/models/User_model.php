cbb30c0508d8e7a0eabd0ae08b68b356138db3c7
Ernie Lail <37405567+MaranathaTech@users.noreply.github.com>
2024-07-14 18:03:02 -0400

    Tidy formatting

--- myapp/application/models/User_model.php
+++ myapp/application/models/User_model.php
@@ -4,58 +4,47 @@
 
 
 		public function __construct(){
-			
 			parent::__construct();
-
 		}
 
 
 		public function validate($email,$pass){
-			
 			$this->db->where('user_email',$email);
 			$this->db->where('user_password',$pass);
 			$result = $this->db->get('users', 1);
 			return $result;
-
 		}
 
 
 		public function checkForUser($email){
-			
 			$this->db->where('user_email',$email);
 			$result = $this->db->get('users', 1);
 			return $result;
-
 		}
 
 
 		public function register($email, $pass){
-    		
-    		$data = array(
-        	'user_email' 	=> $email,
-        	'user_password' => $pass,
-        	'user_level'	=> 3
-    		);
-
-    		$result = $this->db->insert('users',$data);
+	    		$data = array(
+	        	'user_email' 	=> $email,
+	        	'user_password' => $pass,
+	        	'user_level'	=> 3
+	    		);
+	
+	    		$result = $this->db->insert('users',$data);
 			return $result;
-
 		}
 
 
 		public function update($email, $pass, $org){
-    		
-    		$data = array(
-        	'user_password' => $pass,
-    		'user_org' 		=> $org
-    		);
-
-    		$this->db->where('user_email', $email);
-    		$result = $this->db->update('users', $data);
-
-    		return true;
-
+	    		$data = array(
+	        	'user_password' => $pass,
+	    		'user_org' 		=> $org
+	    		);
+	
+	    		$this->db->where('user_email', $email);
+	    		$result = $this->db->update('users', $data);
+	
+	    		return true;
 		}
-
 
 	}
